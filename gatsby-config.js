00bb2d819dc68d9451ef46d35b53b8e07348f94d
Ace Alrich <hex0@live.com>
2020-05-27 18:50:22 +0100

    read env

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,7 @@ module.exports = {
     title: `Gatsby Default Starter`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
+    key: process.env.API_KEY,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
