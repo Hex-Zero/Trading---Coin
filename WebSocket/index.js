36fea83b568b8edbd2da96f4de9f6dc8c5c2b7a6
Ace Alrich <hex0@live.com>
2020-05-28 20:06:05 +0100

    open connection at port and listen

--- WebSocket/index.js
+++ WebSocket/index.js
@@ -1,3 +1,7 @@
 const webSocketsServerPort = 8000
 const webSocketServer = require("websocket").server
 const http = require("http")
+
+const server = http.createServer()
+server.listen(webSocketsServerPort)
+console.log("listenting: 8000")
